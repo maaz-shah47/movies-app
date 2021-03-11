a7e1fcb2fd7211ea4c058408b675c19886512c9f
Syed Maaz Shah <s.tech4747@gmail.com>
2021-03-11 20:58:44 +0500

    Movie app Repository first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+import Movies from "./components/moviesComponent";
+function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
